d5116a33f9eecdcd094bfa2c4215f19477c3c37b
Amrabdo74 <88573123+Amrabdo74@users.noreply.github.com>
2023-08-28 02:08:07 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function btnaddClick() {
     var leftTag = document.createElement("div");
     leftTag.classList.add("left");
     leftTag.innerHTML =
-      "<img class='deleteIcon' src='https://amrabdo74.github.io/images/download.png'/>";
+      "<img class='deleteIcon' src='https://raw.githubusercontent.com/Amrabdo74/To-do/main/images/download.png'/>";
     icons.appendChild(leftTag);
 
     leftTag.onclick = function () {
@@ -81,7 +81,7 @@ todos.forEach((todoItem) => {
 
   var leftTag = document.createElement("div");
   leftTag.classList.add("left");
-  leftTag.innerHTML = "<img class='deleteIcon' src='https://amrabdo74.github.io/images/download.png'/>";
+  leftTag.innerHTML = "<img class='deleteIcon' src='https://raw.githubusercontent.com/Amrabdo74/To-do/main/images/download.png'/>";
   icons.appendChild(leftTag);
 
   leftTag.onclick = function () {
